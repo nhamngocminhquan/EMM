c49e2d2dd78fcbc6085422faf1c97c1e03630295
nhamngocminhquan <mnn7679@nyu.edu>
2022-01-05 03:06:48 +0700

    Updated plotting

--- mars.py
+++ mars.py
@@ -10,7 +10,7 @@ from matplotlib import cm
 import numpy as np
 from os import listdir
 from os.path import isfile, join
-import pickle5 as pickle
+import pickle
 
 # ep.get_sdc_token(user_name='QuanNham', password='irQPmFciRTa6ajP?')
 # print(ep.USER_TOKEN)
@@ -112,18 +112,6 @@ while i < len(em_unixes):
 # print(pairs)
 # [print(em_q[em_unixes[i][1]]['timetag'].partition('+')[0] + ' ' + ex_q[ex_unixes[j][1]]['timetag'].partition('+')[0]) for i, j in pairs]
 
-# Get files
-file_dir = "emm_data/0224"
-file_character = "emr"
-fits_files = [filename for filename in listdir(file_dir) if (isfile(join(file_dir, filename))
-                                                             and file_character in filename)]
-file_character = "exi"
-img_files = [filename for filename in listdir(file_dir) if (isfile(join(file_dir, filename))
-                                                            and file_character in filename)]
-index = 2
-# print(fits_files)
-# print(img_files)
-
 # Time in sclk using conversion diverges 5 days 1 hour from labeled time
 # offset = 435600
 # for file in fits_files:
@@ -138,154 +126,169 @@ index = 2
 #     print((Time(2000, format='jyear') + TimeDelta((data[hdr['NAXIS2'] - 1][0] + offset +
 #                                                    data[hdr['NAXIS2'] - 1][1] * 1.0 / 65536)*u.s)).iso)
 
+# Get files
+file_dir = "emm_data/0224"
+file_character = "emr"
+fits_files = [filename for filename in listdir(file_dir) if (isfile(join(file_dir, filename))
+                                                             and file_character in filename)]
+file_character = "xos1_f635_r_v03-01.fits"
+img_files = [filename for filename in listdir(file_dir) if (isfile(join(file_dir, filename))
+                                                            and file_character in filename)]
+# index = 2
+# print(fits_files)
+# print(img_files)
 
-# hdul = fits.open("emm_emr_l2_20210305t094430_0017_r_v00-01.fits")
-# hdul = fits.open("emm_data/emm_emr_l2_20210308t045230_0019_r_v00-01.fits")
-# hdul = fits.open("emm_data/emm_emr_l2_20210306t192330_0018_r_v00-01.fits")
-# hdul = fits.open("emm_data/emm_emr_l2_20210311t000756_0021_r_v00-01.fits")
+for index in range(len(fits_files)):
+    # hdul = fits.open("emm_emr_l2_20210305t094430_0017_r_v00-01.fits")
+    # hdul = fits.open("emm_data/emm_emr_l2_20210308t045230_0019_r_v00-01.fits")
+    # hdul = fits.open("emm_data/emm_emr_l2_20210306t192330_0018_r_v00-01.fits")
+    # hdul = fits.open("emm_data/emm_emr_l2_20210311t000756_0021_r_v00-01.fits")
 
-# Image view
-hdul = fits.open(join(file_dir, img_files[index]))
-# hdul = fits.open("emm_data/0224/emm_exi_l2a_20210224T095615_0011_xos1_f635_r_v03-01.fits")
-hdul.verify('fix')
-utc = hdul[0].header['DATE-OBS']
-# print(repr(hdr))
+    # Image view
+    hdul = fits.open(join(file_dir, img_files[index]))
+    # hdul = fits.open("emm_data/0224/emm_exi_l2a_20210224T095615_0011_xos1_f635_r_v03-01.fits")
+    hdul.verify('fix')
+    utc = hdul[0].header['DATE-OBS']
+    utc = utc.partition('T')[0] + " " + utc.partition('T')[2]
+    # print(repr(hdr))
 
-# 1: SCI img, 5: PHA, 7: LAT, 8: LONG, 9: HEIGHT, 10: INA
-interest = [1, 5, 7, 8, 9, 10]
-img = [hdul[i].data for i in interest]
-# print(img[4])
-# Fixing data
-hdr = hdul[1].header
-mn = hdr['DATAMIN']
-mx = hdr['DATAMAX']
-w = 2048
-h = 1536
-rg = mx - mn
-mask = np.zeros((h, w), dtype=bool)
-max_altitude = 10
-for i in range(h):
-    for j in range(w):
-        if not np.isnan(img[0][i][j]):
-            if (img[4][i][j] < max_altitude):
-                continue
-                # img[0][i][j] = (img[0][i][j] - mn) * 1.0 / rg
-            else:
-                for k in range(len(interest)):
-                    img[k][i][j] = np.nan
+    # 1: SCI img, 5: PHA, 7: LAT, 8: LONG, 9: HEIGHT, 10: INA
+    interest = [1, 5, 7, 8, 9, 10]
+    img = [hdul[i].data for i in interest]
+    # print(img[4])
+    # Fixing data
+    hdr = hdul[1].header
+    mn = hdr['DATAMIN']
+    mx = hdr['DATAMAX']
+    w = 2048
+    h = 1536
+    rg = mx - mn
+    mask = np.zeros((h, w), dtype=bool)
+    max_altitude = 10
+    for i in range(h):
+        for j in range(w):
+            if not np.isnan(img[0][i][j]):
+                if (img[4][i][j] < max_altitude):
+                    continue
+                    # img[0][i][j] = (img[0][i][j] - mn) * 1.0 / rg
+                else:
+                    for k in range(len(interest)):
+                        img[k][i][j] = np.nan
 
-            # mask[i][j] = 1
-        # else:
-        # img[0][i][j] = 0
+                # mask[i][j] = 1
+            # else:
+            # img[0][i][j] = 0
 
-hdul.close()
-# mn = min(img[0])
-# mx = max(img[0])
-# range = mx - mn
-# img[0] = [(i - mn) * 1.0 / range for i in img[0]]
-# print(img[0])
+    hdul.close()
+    # mn = min(img[0])
+    # mx = max(img[0])
+    # range = mx - mn
+    # img[0] = [(i - mn) * 1.0 / range for i in img[0]]
+    # print(img[0])
 
-fig = plt.figure()
-fig.canvas.manager.full_screen_toggle()
-radius = 1
-azim = 105
-elev = 10
+    fig = plt.figure()
+    fig.canvas.manager.full_screen_toggle()
+    radius = 1
+    azim = 105
+    elev = 10
 
-# Plot help
-# https://stackoverflow.com/a/42927880
-ax = fig.add_subplot(1, 2, 1, projection='3d')
-imgs = np.array(img[0])
-lats = np.array(np.deg2rad(img[2]))
-longs = np.array(np.deg2rad(img[3]))
-norm = matplotlib.colors.Normalize(vmin=mn, vmax=mx)
-sf = ax.plot_surface(radius * np.cos(lats) * np.cos(longs),
-                     radius * np.cos(lats) * np.sin(longs),
-                     radius * np.sin(lats),
-                     rstride=1, cstride=1,
-                     facecolors=cm.gray(norm(imgs)))
-                     # facecolors=cm.gray(imgs))
+    # Plot help
+    # https://stackoverflow.com/a/42927880
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    imgs = np.array(img[0])
+    lats = np.array(np.deg2rad(img[2]))
+    longs = np.array(np.deg2rad(img[3]))
+    norm = matplotlib.colors.Normalize(vmin=mn, vmax=mx)
+    sf = ax.plot_surface(radius * np.cos(lats) * np.cos(longs),
+                         radius * np.cos(lats) * np.sin(longs),
+                         radius * np.sin(lats),
+                         rstride=12, cstride=12,
+                         facecolors=cm.gray(norm(imgs)))
+                         # facecolors=cm.gray(imgs))
 
-ax.set_box_aspect((1, 1, 1))
-ax.set_title("EXI image @ f635 on unit sphere" + "\n" + utc, pad=-10)
-ax.set_xticks(np.arange(-radius, radius, 0.5))
-ax.set_yticks(np.arange(-radius, radius, 0.5))
-ax.set_zticks(np.arange(-radius, radius, 0.5))
-ax.set_xlim(-radius, radius)
-ax.set_ylim(-radius, radius)
-ax.set_zlim(-radius, radius)
-ax.view_init(azim=azim, elev=elev)
-cb = fig.colorbar(cm.ScalarMappable(cmap=cm.gray, norm=norm), ax=ax, shrink=0.5)
-cb.set_label("Calibrated DN", labelpad=10)
-# fig.colorbar(cm.ScalarMappable(cmap=cm.gray, norm=imgs).set_array([]), ax=ax, shrink=0.5)
+    ax.set_box_aspect((1, 1, 1))
+    ax.set_title("EXI image @ f635 on unit sphere" + "\n" + utc, pad=-10)
+    ax.set_xticks(np.arange(-radius, radius, 0.5))
+    ax.set_yticks(np.arange(-radius, radius, 0.5))
+    ax.set_zticks(np.arange(-radius, radius, 0.5))
+    ax.set_xlim(-radius, radius)
+    ax.set_ylim(-radius, radius)
+    ax.set_zlim(-radius, radius)
+    ax.view_init(azim=azim, elev=elev)
+    cb = fig.colorbar(cm.ScalarMappable(cmap=cm.gray, norm=norm), ax=ax, shrink=0.5)
+    cb.set_label("Calibrated DN", labelpad=10)
+    # fig.colorbar(cm.ScalarMappable(cmap=cm.gray, norm=imgs).set_array([]), ax=ax, shrink=0.5)
 
-# axImg = fig.add_subplot(1, 2, 1)
-# XX, YY = np.meshgrid(np.linspace(0, w - 1, w), np.linspace(0, h - 1, h))
-# axImg.imshow(img[0], cmap='gray', vmin=mn, vmax=mx)
-# axImg.imshow(img[4], cmap='gray')
-# axImg.imshow(np.sin(img[2]), cmap='gray')
-# print(min([img[4][i][j] for i in range(h) for j in range(w) if mask[i][j] == 1]))
+    # axImg = fig.add_subplot(1, 2, 1)
+    # XX, YY = np.meshgrid(np.linspace(0, w - 1, w), np.linspace(0, h - 1, h))
+    # axImg.imshow(img[0], cmap='gray', vmin=mn, vmax=mx)
+    # axImg.imshow(img[4], cmap='gray')
+    # axImg.imshow(np.sin(img[2]), cmap='gray')
+    # print(min([img[4][i][j] for i in range(h) for j in range(w) if mask[i][j] == 1]))
 
-# Spectro view
-hdul = fits.open(join(file_dir, fits_files[index]))
-# hdul = fits.open("emm_data/0224/emm_emr_l2_20210224t095920_0011_r_v00-01.fits")
-hdul.verify('fix')
-hdr = hdul[1].header
-# print(repr(hdr))
-data = hdul[1].data
-hdul.close()
-# print(data)
+    # Spectro view
+    hdul = fits.open(join(file_dir, fits_files[index]))
+    # hdul = fits.open("emm_data/0224/emm_emr_l2_20210224t095920_0011_r_v00-01.fits")
+    hdul.verify('fix')
+    hdr = hdul[1].header
+    # print(repr(hdr))
+    data = hdul[1].data
+    utc = data[0][9]
+    utc2 = data[-1][9]
+    hdul.close()
+    # print(data)
 
-# Field 3 is calibrated radiance
-# print(data[5][3])
+    # Field 3 is calibrated radiance
+    # print(data[5][3])
 
-# Field 7 is max brightness temp
-# Fields 21 and 22 are latitudes and longitudes of IFOV
-ax = fig.add_subplot(1, 2, 2, projection='3d')
-dstgui = []
-for i in range(len(data)):
-    if data[i][7] > 0.001 and (abs(data[i][21]) > 0.001 or abs(data[i][22]) > 0.001):
-        dstgui.append(1)
-    else:
-        dstgui.append(0)
+    # Field 7 is max brightness temp
+    # Fields 21 and 22 are latitudes and longitudes of IFOV
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    dstgui = []
+    for i in range(len(data)):
+        if data[i][7] > 0.001 and (abs(data[i][21]) > 0.001 or abs(data[i][22]) > 0.001):
+            dstgui.append(1)
+        else:
+            dstgui.append(0)
 
-# max_temps = [reading[7] for reading in data]
-# lats = [np.deg2rad(reading[21]) for reading in data]
-# longs = [np.deg2rad(reading[22]) for reading in data]
-max_temps = [data[i][7] for i in range(len(data)) if dstgui[i] == 1]
-lats = [np.deg2rad(data[i][21]) for i in range(len(data)) if dstgui[i] == 1]
-longs = [np.deg2rad(data[i][22]) for i in range(len(data)) if dstgui[i] == 1]
+    # max_temps = [reading[7] for reading in data]
+    # lats = [np.deg2rad(reading[21]) for reading in data]
+    # longs = [np.deg2rad(reading[22]) for reading in data]
+    max_temps = [data[i][7] for i in range(len(data)) if dstgui[i] == 1]
+    lats = [np.deg2rad(data[i][21]) for i in range(len(data)) if dstgui[i] == 1]
+    longs = [np.deg2rad(data[i][22]) for i in range(len(data)) if dstgui[i] == 1]
 
-sc = ax.scatter(np.array(radius * np.cos(lats) * np.cos(longs)),
-                np.array(radius * np.cos(lats) * np.sin(longs)),
-                np.array(radius * np.sin(lats)),
-                c=max_temps, cmap='plasma', marker='o')
+    sc = ax.scatter(np.array(radius * np.cos(lats) * np.cos(longs)),
+                    np.array(radius * np.cos(lats) * np.sin(longs)),
+                    np.array(radius * np.sin(lats)),
+                    c=max_temps, cmap='plasma', marker='o')
 
-# ax.plot(np.array(radius * np.cos(lats) * np.cos(longs)),
-#            np.array(radius * np.cos(lats) * np.sin(longs)),
-#            np.array(radius * np.sin(lats)))
+    # ax.plot(np.array(radius * np.cos(lats) * np.cos(longs)),
+    #            np.array(radius * np.cos(lats) * np.sin(longs)),
+    #            np.array(radius * np.sin(lats)))
 
-ax.set_box_aspect((1, 1, 1))
-ax.set_title("EMIR max brightness on unit sphere" + "\n" + utc, pad=-10)
-ax.set_xticks(np.arange(-radius, radius, 0.5))
-ax.set_yticks(np.arange(-radius, radius, 0.5))
-ax.set_zticks(np.arange(-radius, radius, 0.5))
-ax.set_xlim(-radius, radius)
-ax.set_ylim(-radius, radius)
-ax.set_zlim(-radius, radius)
-ax.view_init(azim=azim, elev=elev)
-cb = fig.colorbar(sc, ax=ax, shrink=0.5)
-cb.set_label("Max brightness temperature (K)", labelpad=10)
+    ax.set_box_aspect((1, 1, 1))
+    ax.set_title("EMIRS max brightness on unit sphere" + "\n" + utc + " -\n" + utc2, pad=-10)
+    ax.set_xticks(np.arange(-radius, radius, 0.5))
+    ax.set_yticks(np.arange(-radius, radius, 0.5))
+    ax.set_zticks(np.arange(-radius, radius, 0.5))
+    ax.set_xlim(-radius, radius)
+    ax.set_ylim(-radius, radius)
+    ax.set_zlim(-radius, radius)
+    ax.view_init(azim=azim, elev=elev)
+    cb = fig.colorbar(sc, ax=ax, shrink=0.5)
+    cb.set_label("Max brightness temperature (K)", labelpad=10)
 
-# ax = fig.add_subplot(1, 1, 1)
-# max_temps = np.array([reading[7] for reading in data])
-# lats = np.array([reading[21] for reading in data])
-# longs = np.array([reading[22] for reading in data])
-# ax.scatter(np.array(lats), np.array(longs), c=max_temps, cmap='plasma', alpha=1, s=np.array(dstgui)*10)
-# # for i in range(hdr['NAXIS2']):
-# #     ax.scatter(data[i][21], data[i][22], alpha=0.5, s=1.5)
+    # ax = fig.add_subplot(1, 1, 1)
+    # max_temps = np.array([reading[7] for reading in data])
+    # lats = np.array([reading[21] for reading in data])
+    # longs = np.array([reading[22] for reading in data])
+    # ax.scatter(np.array(lats), np.array(longs), c=max_temps, cmap='plasma', alpha=1, s=np.array(dstgui)*10)
+    # # for i in range(hdr['NAXIS2']):
+    # #     ax.scatter(data[i][21], data[i][22], alpha=0.5, s=1.5)
 
-# plt.show() # block=True
-plt.savefig("6_{}".format(index), dpi=200)
+    # plt.show() # block=True
+    plt.savefig("6_{}".format(index), dpi=200)
 
 # Find time
 # Time in utc field diverges 3 mins from labeled time
